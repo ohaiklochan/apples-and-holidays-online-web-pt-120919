720dadd5836164cd7a536b062066f65bd6b68dc7
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 04:11:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
+    holiday_hash.each do |
 end
 
 def all_holidays_with_bbq(holiday_hash)
